e7b02687c33af8f885a35d2d79d0626da32c29c5
Clint Reynolds <clint.reynolds@smsportal.com>
2024-06-27 21:33:36 +0200

    Adds transaction queue.
    - Adds transaction queue experiment and unit tests.

---
+++ Experiments.Tests/TransactionQueueTest.cs
@@ -0,0 +1,292 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Experiments.Tests
+{
+    public class TransactionQueueTest
+    {
+	    private TransactionQueue<int> queue;
+
+		[SetUp]
+		public void Setup()
+		{
+			queue = new TransactionQueue<int>();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			queue = null;
+		}
+
+		[Test]
+		public void QueueCountStartsAtZero()
+		{
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void CanEnqueueItem()
+		{
+			// Arrange / Act
+			queue.Enqueue(1);
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void CloseTransactionOnLastItemYieldsZeroQueueSize()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.CreateTransaction( out var key);
+			
+			// Act
+			queue.CloseTransaction(key);
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void CreateTransactionReturnsCorrectKey()
+		{
+			// Arrange
+			var key1 = queue.Enqueue(1);
+			
+			// Act
+			queue.CreateTransaction( out var key2);
+
+			// Assert
+			Assert.That(key1, Is.EqualTo(key2));
+		}
+
+		[Test]
+		public void CanUpdateValueInTransactionQueue()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.CreateTransaction( out var key);
+			
+			// Act
+			queue.UpdateTransaction(key, 2);
+			queue.DiscardTransaction(key);
+			var item = queue.CreateTransaction( out _);
+
+			// Assert
+			Assert.That(item, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void CanEnqueueMultipleItems()
+		{
+			// Arrange / Act
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+
+			Assert.That(queue.Count, Is.EqualTo(4));
+		}
+
+		[Test]
+		public void KeyReturnedFromEnqueueEqualsQueueCount()
+		{
+			// Arrange
+			queue.Enqueue(10);
+			queue.Enqueue(11);
+			
+			// Act
+			var key = queue.Enqueue(12);
+
+			Assert.That(key, Is.EqualTo(queue.Count));
+		}
+
+		[Test]
+		public void CanReserveItem()
+		{
+			// Arrange
+			queue.Enqueue(1);
+
+			// Act
+			var item = queue.CreateTransaction(out _);
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(0));
+			Assert.That(queue.TransactionCount, Is.EqualTo(1));
+			Assert.That(item, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void CanReserveMultipleItemsIndividually()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+
+			// Act / Assert
+			var item = queue.CreateTransaction(out _);
+			Assert.That(item, Is.EqualTo(1));
+
+			item = queue.CreateTransaction(out _);
+			Assert.That(item, Is.EqualTo(2));
+
+			item = queue.CreateTransaction(out _);
+			Assert.That(item, Is.EqualTo(3));
+
+			Assert.That(queue.Count, Is.EqualTo(1));
+			Assert.That(queue.TransactionCount, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void ClearResetsCounts()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+
+			queue.CreateTransaction(out _);
+			queue.CreateTransaction(out _);
+
+			// Act
+			queue.Clear();
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(0));
+			Assert.That(queue.TransactionCount, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void CanUnreserveItem()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.CreateTransaction(out var key);
+
+			// Act
+			queue.DiscardTransaction(key);
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(1));
+			Assert.That(queue.TransactionCount, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void CanRemoveItem()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.CreateTransaction(out var key);
+			
+			// Act
+			queue.CloseTransaction(key);
+
+			// Assert
+			Assert.That(queue.Count, Is.EqualTo(0));
+			Assert.That(queue.TransactionCount, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void CreateTransactionPullFromFrontOfQueue()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			
+			// Act
+			var item = queue.CreateTransaction(out var key);
+
+			// Assert
+			Assert.That(item, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void DiscardTransactionAddsToBackOfQueue()
+		{
+			// Arrange
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			
+			// Act
+			var item = queue.CreateTransaction(out var key);
+			queue.DiscardTransaction(key); // adds first item to the back of the queue
+	
+			// Assert
+			item = queue.CreateTransaction(out key); // reserves next item in queue
+			Assert.That(item, Is.EqualTo(2));
+			queue.CloseTransaction(key);
+
+			item = queue.CreateTransaction(out key); // reserves last item in queue
+			Assert.That(item, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void CanEnqueueOnMultipleThreads()
+		{
+			const int threadCount = 16;
+			const ulong iterations = 1000 * 100;
+
+			var threads = new List<Thread>();
+			var tcs = new TaskCompletionSource<int>();
+
+			for (var i = 0; i < threadCount; i++)
+			{
+				threads.Add(new Thread(s =>
+				{
+					tcs.Task.Wait();
+					for (ulong j = 0; j < iterations; j++)
+					{
+						queue.Enqueue(1);
+					}
+				}));
+			}
+
+			threads.ForEach(t => t.Start());
+			tcs.SetResult(0);
+			threads.ForEach(t => t.Join());
+
+			Assert.That(queue.Count, Is.EqualTo(threadCount * (int)iterations));
+		}
+
+		[Test]
+		public void OperationsReadWriteThreadSafe()
+		{
+			const int threadCount = 16;
+			const ulong iterations = 1000 * 100;
+
+			var threads = new List<Thread>();
+			var tcs = new TaskCompletionSource<int>();
+
+			for (var i = 0; i < threadCount; i++)
+			{
+				threads.Add(new Thread(s =>
+				{
+					tcs.Task.Wait();
+					for (ulong j = 0; j < iterations; j++)
+					{
+						queue.Enqueue(1);
+						var value = queue.CreateTransaction(out var key);
+						queue.UpdateTransaction(key, value);
+						queue.DiscardTransaction(key);
+						queue.CreateTransaction(out key);
+						queue.CloseTransaction(key);
+					}
+				}));
+			}
+
+			threads.ForEach(t => t.Start());
+			tcs.SetResult(0);
+			threads.ForEach(t => t.Join());
+
+			Assert.That(queue.Count, Is.EqualTo(0));
+		}
+	}
+}
---
+++ Experiments/TransactionQueue.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+
+namespace Experiments
+{
+	/// <summary>
+	/// Represents a queue whose items can be reserved through the creation of a transaction. 
+	/// </summary>
+	/// <typeparam name="TValue">The type of item to be added to the queue.</typeparam>
+	/// <remarks>
+	/// This is the creation of a thought experiment. We wanted to know if we could create a queue where we could
+	/// reserve an item on the queue, not remove it, process the item and remove it from the queue once completed
+	/// processing.
+	///
+	/// We also wanted to be able to re-add the item back onto the queue if we could not process it at that time
+	/// for some reason which would allow the item to be processed at a later point.
+	///
+	/// It works by containing both a queue to store items in the queue and also by containing a dictionary of
+	/// all transactions that have been created. When a transaction is created it is removed from the queue and added
+	/// to the collection of transactions. Each transaction contains a key associated with it and which is used to
+	/// close or discard a transaction or update the value associated with the transaction i.e. the queue item.
+	/// </remarks>
+	public class TransactionQueue<TValue>
+	{
+		private ulong keyCounter;
+		
+		private readonly object locker = new();
+		private readonly Queue<Item> queue = new();
+		private readonly Dictionary<ulong, Item> transactions = new();
+
+		/// <summary>
+		/// The total number of items in the queue.
+		/// </summary>
+		public int Count 
+		{
+			get
+			{
+				lock (locker)
+				{
+					return queue.Count;
+				}
+			}
+		}
+
+		/// <summary>
+		/// The total number of currently created transactions.
+		/// </summary>
+		public int TransactionCount
+		{
+			get
+			{
+				lock (locker)
+				{
+					return transactions.Count;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Add a value to the end of the queue.
+		/// </summary>
+		public ulong Enqueue(TValue value)
+		{
+			lock (locker)
+			{
+				var item = new Item(++keyCounter, value);
+				queue.Enqueue(item);
+			
+				return keyCounter;
+			}
+		}
+
+		/// <summary>
+		/// Remove a value from the queue and place it in the transaction collection.
+		/// </summary>
+		public TValue CreateTransaction(out ulong key)
+		{
+			lock (locker)
+			{
+				var item = queue.Dequeue();
+				transactions.Add(item.Key, item);
+			
+				key = item.Key;
+				return item.Value;
+			}
+		}
+
+		/// <summary>
+		/// Update the value in the transaction collection.
+		/// </summary>
+		public void UpdateTransaction(ulong key, TValue value)
+		{
+			lock (locker)
+			{
+				transactions[key].Value = value;
+			}
+		}
+
+		/// <summary>
+		/// Put the value associated with the key back onto the queue for processing and remove it from the transaction collection.
+		/// </summary>
+		public void DiscardTransaction(ulong key)
+		{
+			lock (locker)
+			{
+				var item = transactions[key];
+				queue.Enqueue(item);
+			
+				transactions.Remove(key);
+			}
+		}
+
+		/// <summary>
+		/// Remove the value from the transaction collection.
+		/// </summary>
+		public void CloseTransaction(ulong key)
+		{
+			lock (locker)
+			{
+				transactions.Remove(key);	
+			}
+		}
+
+		/// <summary>
+		/// Removes all objects from the collection.
+		/// </summary>
+		public void Clear()
+		{
+			lock (locker)
+			{
+				keyCounter = 0;
+				queue.Clear();
+				transactions.Clear();	
+			}
+		}
+
+		private class Item(ulong key, TValue value)
+		{
+			public ulong Key { get; } = key;
+
+			public TValue Value { get; set; } = value;
+		}
+	}
+}
